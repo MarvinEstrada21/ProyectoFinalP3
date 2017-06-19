76a1443cb8b90483ff7d28fa4d3410f716c5ac20
marvinestrada21 <marvinestrada2@gmail.com>
2017-06-18 22:09:59 -0600

    Taquilla

---
+++ Taquilla.h
@@ -0,0 +1,15 @@
+#include <string>
+
+#ifndef TAQUILLA_H
+#define TAQUILLA_H
+
+class Taquilla {
+	private:
+		double precio;
+	public:
+		Taquilla();
+		Taquilla(double);
+
+		double getPrecio(char[], int);
+};
+#endif
